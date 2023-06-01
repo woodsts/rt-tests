76d3076b5773b0e99c39184bbfc8d4a7ce182b0b
Marcelo Tosatti <mtosatti@redhat.com>
2023-06-01 17:04:50 -0300

    oslat: remove newline from tracemark message
    Remove the newline from the trace mark message: it breaks
    tools that expect the trace files to contain the standard
    trace format on each new trace line.
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- src/oslat/oslat.c
+++ src/oslat/oslat.c
@@ -337,8 +337,7 @@ static void insert_bucket(struct thread *t, stamp_t value)
 	lat = (value * g.unit_per_us + t->counter_mhz - 1) / t->counter_mhz;
 	us = (double)lat / g.unit_per_us;
 	if (!g.preheat && g.trace_threshold && us >= g.trace_threshold) {
-		char *line = "%s: Trace threshold (%d us) triggered on cpu %d with %.*f us!\n"
-		    "Stopping the test.\n";
+		char *line = "%s: Trace threshold (%d us) triggered on cpu %d with %.*f us!\n";
 		tracemark(line, g.app_name, g.trace_threshold, t->core_i,
 			  g.precision, us);
 		err_quit(line, g.app_name, g.trace_threshold, t->core_i,
