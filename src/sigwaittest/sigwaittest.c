33a898a8b31c4bc41406599d72e8f76b47c7438d
John Kacur <jkacur@redhat.com>
2023-10-06 11:55:37 -0400

    rt-tests: sigwaittest: Fix some warnings
    Fix some warnings about unused parameters and comparisons of integers of
    different signedness
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- src/sigwaittest/sigwaittest.c
+++ src/sigwaittest/sigwaittest.c
@@ -384,13 +384,13 @@ static void process_options(int argc, char *argv[])
 
 static int volatile mustshutdown;
 
-static void sighand(int sig)
+static void sighand(int sig __attribute__ ((unused)))
 {
 	mustshutdown = 1;
 }
 
 static void print_stat(FILE *fp, struct params *receiver, struct params *sender,
-		       int verbose, int quiet)
+		       int verbose __attribute__ ((unused)), int quiet)
 {
 	int i;
 
@@ -435,7 +435,7 @@ static void write_stats(FILE *f, void *data)
 {
 	struct params_stats *ps = data;
 	struct params *s, *r;
-	unsigned int i;
+	int i;
 
 	fprintf(f, "  \"num_threads\": %d,\n", num_threads);
 	fprintf(f, "  \"thread\": {\n");
