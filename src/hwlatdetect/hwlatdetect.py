woodsts/rt-tests

commit 96e78093832288b705c507b1ee443aeffe126bba

rt-tests: hwlatdetect: Add field cpu to samples output
Information about CPU index is valuable for debugging
latency issues

Signed-off-by: John Kacur <jkacur@redhat.com>

src/hwlatdetect/hwlatdetect.py
@@ -237,10 +237,11 @@ class Tracer(Detector):
 
     class Sample:
         'private class for tracer sample data'
-        __slots__ = 'timestamp', 'inner', 'outer'
+        __slots__ = 'cpu', 'timestamp', 'inner', 'outer'
 
         def __init__(self, line):
             fields = line.split()
+            self.cpu = int(fields[1][1:-1])
             i, o = fields[6].split('/')
             ts = fields[7][3:]
             self.timestamp = str(ts)
@@ -248,7 +249,7 @@ class Tracer(Detector):
             self.outer = int(o)
 
         def __str__(self):
-            return f"ts: {self.timestamp}, inner:{self.inner}, outer:{self.outer}"
+            return f"ts: {self.timestamp}, inner:{self.inner}, outer:{self.outer}, cpu:{self.cpu}"
 
         def display(self):
             """ convert object to string and print """

commit 1944197a3251fcf282a93eb0768b72c2c3c4bc48

hwlatdetect: correctly get CPU number from different sample formats
Make hwlatdetect able to get the CPU number from samples in both
'nolatency-format' (currently covered by the code) and also from
samples in 'latency-format'.

With this change the code is now able to get the CPU from samples
in the following formats:

- nolatency-format:
   <...>-3820    [007] d.... 22851.463472: #2     inner/outer(us):    0/233   ts:1770937572.925294075 count:1
   <...>-3820    [011] d.... 22855.495526: #3     inner/outer(us):  308/305   ts:1770937576.805213360 count:49

- latency-format:
   <...>-3835     13d.... 12608167us : #2     inner/outer(us):  307/299   ts:1770937629.988530678 count:49
   <...>-3835     16d.... 15632212us : #3     inner/outer(us):  308/296   ts:1770937633.322710076 count:24

Fixes: 96e780938322 ("rt-tests: hwlatdetect: Add field cpu to samples output")
Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>
Signed-off-by: John Kacur <jkacur@redhat.com>

src/hwlatdetect/hwlatdetect.py
@@ -258,7 +258,7 @@ class Tracer(Detector):
         def __init__(self, line):
             fields = line.split()
             kv = key_values(fields)
-            self.cpu = int(fields[1][1:-1])
+            self.cpu = int(fields[1][1:-1]) if fields[1][0] == '[' else int(fields[1][:-5])
             i, o = fields[6].split('/')
             ts = fields[7][3:]
             self.timestamp = str(ts)