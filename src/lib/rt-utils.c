woodsts/rt-tests

commit 60d60540c0094d30e2faa7a36037fe483b5a2316

rt-utils.c: Check return values of write()
Add error handling for write() to tracefs in tracemark() to avoid ignoring
partial or failed writes.

Signed-off-by: Cheng-Yang Chou <yphbchou0911@gmail.com>
- resolved merge conflict because an earlier patch changed tracemark()
  and tracing_stop() since this was submitted
Signed-off-by: John Kacur <jkacur@redhat.com>

src/lib/rt-utils.c
@@ -424,6 +424,7 @@ void tracemark(char *fmt, ...)
 {
 	va_list ap;
 	int len;
+	int ret;
 
 	/* bail out if we're not tracing */
 	/* or if the kernel doesn't support trace_mark */
@@ -435,14 +436,20 @@ void tracemark(char *fmt, ...)
 	va_end(ap);
 
 	/* write the tracemark message */
-	write(tracemark_fd, tracebuf, len);
+	ret = write(tracemark_fd, tracebuf, len);
+	if (ret != 2)
+		warn("trace stop write failed");
 }
 
 void tracing_stop(void)
 {
+	int ret;
+
 	if (trace_fd < 0)
 		return;
-	write(trace_fd, "0\n", 2);
+	ret = write(trace_fd, "0\n", 2);
+	if (ret != 2)
+		warn("trace stop write failed");
 }
 
 void enable_trace_mark(void)

commit eb0051851f5b8d1eac9b4ec711da9795586762c2

rt-utils.c: Fix write() return value check
The write() return value check in tracemark() is a copy-paste from the
one in tracing_stop() which checks for 2 bytes to be written. As a
result, tracemark() is almost always warning.

Check for the write() return value to be equal to the written length,
which is the value returned if no error occurred.
While at it, add "\n" to the warn messages.

Fixes: 60d60540c009 ("rt-utils.c: Check return values of write()")
Signed-off-by: Gabriele Monaco <gmonaco@redhat.com>
Signed-off-by: John Kacur <jkacur@redhat.com>

src/lib/rt-utils.c
@@ -437,8 +437,8 @@ void tracemark(char *fmt, ...)
 
 	/* write the tracemark message */
 	ret = write(tracemark_fd, tracebuf, len);
-	if (ret != 2)
-		warn("trace stop write failed");
+	if (ret != len)
+		warn("trace mark write failed\n");
 }
 
 void tracing_stop(void)
@@ -449,7 +449,7 @@ void tracing_stop(void)
 		return;
 	ret = write(trace_fd, "0\n", 2);
 	if (ret != 2)
-		warn("trace stop write failed");
+		warn("trace stop write failed\n");
 }
 
 void enable_trace_mark(void)